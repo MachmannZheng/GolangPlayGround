7c7c6cfc6e392bb8656c14ac05edf1b39a2e0ec5
MachmannZheng <18651125503@163.com>
2024-01-16 23:30:04 +0800

    compelte csv read function

--- fileOperation/CSV/csvRead.go
+++ fileOperation/CSV/csvRead.go
@@ -1,7 +1,65 @@
 package CSV
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
-func readCSV() {
-	fmt.Println("read CSV")
+type ShoppingRecord struct {
+	Vegetable string
+	Fruit     string
+}
+
+func createShoppingList(data [][]string) []ShoppingRecord {
+	var shoppingList []ShoppingRecord
+	for i, line := range data {
+		if i > 0 { // omit header line
+			var rec ShoppingRecord
+			for j, field := range line {
+				if j == 0 {
+					rec.Vegetable = field
+				} else if j == 1 {
+					rec.Fruit = field
+				}
+			}
+			shoppingList = append(shoppingList, rec)
+		}
+	}
+	return shoppingList
+}
+
+func ReadCSV() {
+	// open file
+	f, err := os.Open("./fileOperation/CSV/data.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// remember to close the file at the end of the program
+	defer f.Close()
+
+	// read csv values using csv.Reader
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// convert records to array of structs
+	shoppingList := createShoppingList(data)
+
+	// print the array
+	fmt.Printf("%+v\n", shoppingList)
+
+	for _, val := range shoppingList {
+		fmt.Println(val)
+		b1, _ := strconv.ParseFloat(val.Vegetable, 32)
+		fmt.Println(b1)
+		b2, _ := strconv.ParseFloat(val.Fruit, 32)
+		fmt.Println(b2)
+
+	}
 }
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/MachmannZheng/GolangPlayGround/fileOperation/CSV"
 	"github.com/MachmannZheng/GolangPlayGround/testDelete"
 )
 
 func main() {
 	fmt.Println("the main function is running")
 	testDelete.ReadCSV1()
+	CSV.ReadCSV()
 }
